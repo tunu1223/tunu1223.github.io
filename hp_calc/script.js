1902dd341885945e562930ab6592d5ebf50b5557
tunu1223 <tunu1223@yahoo.co.jp>
2026-07-29 00:06:30 +0900

    修正

--- hp_calc/script.js
+++ hp_calc/script.js
@@ -1,6 +1,8 @@
-const pokeData = await fetch("poke.json").json();
+let response = await fetch("poke.json");
+const pokeData = await response.json();
 
-const tempData = await fetch("templates.json").json();
+response = await fetch("templates.json");
+const tempData = await response.json();
 const templates = {0: "テンプレート選択...", tempData};
 
 const divOptions = [2, 3, 4, 6, 8, 10, 16, 32, 100, 4096];
