7c9df2373d84109ad319799314223fd19613d2dc
tunu1223 <tunu1223@yahoo.co.jp>
2026-07-31 19:16:44 +0900

    a

--- iv_calc/script.js
+++ iv_calc/script.js
@@ -6,3 +6,19 @@ response = await fetch("nature.json");
 const natures = await response.json();
 response = await fetch("types.json");
 const types = await response.json();
+
+const resultBody = document.getElementById("resultBody");
+const habcds = ["h","a","b","c","d","s"];
+for (let i = 31; i >= 0; i--) {
+  let row = document.createElement("tr");
+  let iv = document.createElement("th");
+  iv.innerText = i;
+  row.appendChild(iv);
+  habcds.forEach(habcd => {
+    let h = document.createElement("td");
+    h.id = habcd + i;
+    h.innerText = "-";
+    row.appendChild(h);
+  });
+  resultBody.appendChild(row)
+}
